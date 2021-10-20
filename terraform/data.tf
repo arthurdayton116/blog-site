e5cffe073b22e5e15bc772484809b7f80b9753a5
Arthur Dayton <arthur@arthurdayton.com>
2021-10-20 14:54:39 -0700

    add terraform steps

---
+++ terraform/data.tf
@@ -0,0 +1,27 @@
+variable "base_tags" {
+  default = {
+    owner       = "blog-site"
+    createdBy   = "terraform"
+    environment = "dev"
+    billTo      = "study"
+  }
+  description = "base resource tags"
+  type        = map(string)
+}
+
+variable "resource_prefix" {
+  description = "Common prefix for resource names"
+  default     = "blog-site"
+}
+
+variable "region" {
+  description = "Where resources will be deployed"
+  default     = "us-west-2"
+}
+
+locals {
+  base_tags=var.base_tags
+  resource_prefix=var.resource_prefix
+  region=var.region
+  bucket_name="arthurneedsadomain"
+}
