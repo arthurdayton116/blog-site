85dec181068f144f2050d179787c536e0c27ab2d
Arthur Dayton <arthur@arthurdayton.com>
2021-10-20 20:34:32 -0700

    set up buckets with policies

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,6 +1,15 @@
-resource "aws_s3_bucket" "blog_domain" {
-  bucket        = local.bucket_name
-  acl           = "public-read"
+// root domain bucket and policy
+resource "aws_s3_bucket" "a" {
+  bucket = local.bucket_name
+  acl    = "public-read"
+
+  policy = file("policy.json")
+
+  website {
+    index_document = "index.html"
+    error_document = "index.html"
+  }
+
   force_destroy = false
   tags = merge(
     local.base_tags,
@@ -11,10 +20,42 @@ resource "aws_s3_bucket" "blog_domain" {
   )
 }
 
-resource "aws_s3_bucket" "blog_domain_redirect" {
+resource "aws_s3_bucket_policy" "a" {
+  bucket = aws_s3_bucket.a.id
+
+  # Terraform's "jsonencode" function converts a
+  # Terraform expression's result to valid JSON syntax.
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Id      = "${local.resource_prefix}-${local.bucket_name}-policy"
+    Statement = [
+      {
+        Sid       = "PublicRead"
+        Effect    = "Allow"
+        Principal = "*"
+        Action = [
+          "s3:GetObject",
+          "s3:GetObjectVersion"
+        ]
+        Resource = [
+          aws_s3_bucket.a.arn,
+          "${aws_s3_bucket.a.arn}/*",
+        ]
+      },
+    ]
+  })
+}
+
+// redirect bucket and policy
+resource "aws_s3_bucket" "b" {
   bucket        = local.redirect_bucket_name
   acl           = "public-read"
   force_destroy = false
+
+  website {
+    redirect_all_requests_to = "https://${local.bucket_name}"
+  }
+
   tags = merge(
     local.base_tags,
     {
@@ -22,4 +63,30 @@ resource "aws_s3_bucket" "blog_domain_redirect" {
       directory = basename(path.cwd)
     },
   )
+}
+
+resource "aws_s3_bucket_policy" "b" {
+  bucket = aws_s3_bucket.b.id
+
+  # Terraform's "jsonencode" function converts a
+  # Terraform expression's result to valid JSON syntax.
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Id      = "${local.resource_prefix}-${local.redirect_bucket_name}-policy"
+    Statement = [
+      {
+        Sid       = "PublicRead"
+        Effect    = "Allow"
+        Principal = "*"
+        Action = [
+          "s3:GetObject",
+          "s3:GetObjectVersion"
+        ]
+        Resource = [
+          aws_s3_bucket.b.arn,
+          "${aws_s3_bucket.b.arn}/*",
+        ]
+      },
+    ]
+  })
 }
