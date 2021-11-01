bbe0169b2cbff82b932e7d4c2686f454e9683f85
Arthur Dayton <arthur@arthurdayton.com>
2021-11-01 14:47:04 -0700

    change creation of var for REACT_APP_GRAPHQL_ENDPOINT

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ import {
     ApolloProvider
 } from "@apollo/client";
 
-const GRAPHQL_ENDPOINT = `https://${process.env.GRAPHQL_ENDPOINT}` || 'http://localhost:4000/graphql'
+const GRAPHQL_ENDPOINT = `https://${process.env.REACT_APP_GRAPHQL_ENDPOINT}` || 'http://localhost:4000/graphql'
 const client = new ApolloClient({
     uri: GRAPHQL_ENDPOINT,
     cache: new InMemoryCache()
