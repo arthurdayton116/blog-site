6b2467f6b93172e015535009c40ae21b73bfe8d5
Arthur Dayton <arthur@arthurdayton.com>
2021-11-02 21:02:06 -0700

    update comment box and date format

--- src/components/Comments.js
+++ src/components/Comments.js
@@ -2,9 +2,8 @@ import React, {useEffect, useState} from 'react'
 import ClipLoader from "react-spinners/ClipLoader";
 import {Box, Heading, Flex, Button } from 'rebass';
 import { Label, Input, Textarea } from '@rebass/forms'
-import { FaAngleDown,FaAngleUp } from "react-icons/fa";
+import {FaAngleDown, FaAngleUp, FaMinus, FaPlus} from "react-icons/fa";
 
-// import data from '../blogs/data.json';
 import {useTheme} from "@emotion/react";
 import {gql, useQuery, useMutation} from "@apollo/client";
 // const maxPostNumber = 5
@@ -21,66 +20,6 @@ const GET_COMMENTS = gql`
         }
     }
 `;
-
-// function ExampleQueryFetch() {
-//     const { loading, error, data } = useQuery(GET_COMMENTS, { variables:{"postid": '9999'} });
-//
-//     if (loading) return <p>Loading...</p>;
-//     if (error) return <p>Error :(</p>;
-//     console.log(data.comment)
-//
-//     const { CommentsTableHashKey, postid, name, comment, timestamp } = data.comment;
-//     return (
-//         <div key={CommentsTableHashKey}>
-//             <p>
-//                 {postid}: {name} : {timestamp}
-//             </p>
-//             <p>
-//                 {comment}
-//             </p>
-//         </div>
-//     );
-// }
-
-
-
-
-// const postComment = async (event, cb, obj) => {
-//
-//     const URL = 'https://api.arthurneedsadomain.com/';
-//     const sampleData = {"name":obj.name, "age": 30};
-//
-//         console.log(URL)
-//         console.log(sampleData)
-//     console.log(obj)
-
-//     await fetch(URL, {
-//             method: 'POST', // *GET, POST, PUT, DELETE, etc.
-//             mode: 'cors', // no-cors, *cors, same-origin
-//             cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
-//             credentials: 'omit', // include, same-origin, omit
-//             headers: {
-//                 'Content-Type': 'application/json'
-//                 // 'Content-Type': 'application/x-www-form-urlencoded',
-//             },
-//             redirect: 'follow', // manual, *follow, error
-//             referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
-//             body: JSON.stringify(obj) // body data type must match "Content-Type" header
-//         })
-//            .then(response => {console.log('got hear',response); return response.text()})
-//            .then(data => {
-//                console.log('Success:', data);
-//                cb()
-//            })
-//            .catch((error) => {
-//                console.error('Error:', error);
-//            });
-//
-//
-// }
-
-
-
 
 
 const Comments = (props) => {
@@ -117,15 +56,18 @@ const Comments = (props) => {
 
                 {showComments &&
                 <Box>
-                <Box mt={2} mb={2} sx={theme.h1Sx}>New Comment
-                    {!showForm && <FaAngleDown onClick={() => setshowForm(true)} />}
-                    {showForm && <FaAngleUp onClick={() => setshowForm(false)} />}
+                <Box mt={2} mb={2} sx={theme.h1Sx}>
+                    {!showForm && <FaPlus onClick={() => setshowForm(true)} />}
+                    {showForm && <FaMinus onClick={() => setshowForm(false)} />}
                 </Box>
                     {showForm &&
-                    <RBForm postID={postid}/>}
+                    <RBForm postID={postid} hide={()=>setshowForm(false)}/>}
 
                 <Box>
-                    {data.comments.map(({CommentsTableHashKey, postid, name, comment, timestamp}) => (
+                    {data.comments.map(({CommentsTableHashKey, postid, name, comment, timestamp}) => {
+                        const dtcnvrt = new Date(timestamp)
+                        const dtfmt = dtcnvrt.toString(dtcnvrt)
+                        return (
                         <Box key={postid}>
                             {/*<Box sx={theme.linkSXAlt1}>*/}
                             <Box width={1 / 3} px={0}>
@@ -141,7 +83,7 @@ const Comments = (props) => {
                                             }}
                                             id={'date-' + CommentsTableHashKey}
                                             name='date'
-                                            defaultValue={timestamp}
+                                            defaultValue={dtfmt}
                                             disabled
                                         />
                                     </Box>
@@ -170,7 +112,9 @@ const Comments = (props) => {
                                 </Flex>
                             </Box>
                         </Box>
-                    ))}
+                    )
+                    }
+                    )}
                 </Box>
                 </Box>
                 }
@@ -203,7 +147,7 @@ function RBForm (props){
     const currentDateTime = Date().toLocaleString();
     const theme = useTheme()
     return (
-        <Box p={2} bg={theme.colors.commentBG}>
+        <Box p={2} >
         <Box
              as='form'
             onSubmit={async e => {
@@ -217,7 +161,7 @@ function RBForm (props){
                 console.log(name)
                 console.log(comment)
                 console.log(e.target[3].value)
-                // setCommentIsPosting(true)
+
                 await addComment(
                 {variables:{
                     "comment": {
@@ -228,11 +172,8 @@ function RBForm (props){
                     }
                 }
                 })
-                // setCommentIsPosting(false)
+                props.hide()
 
-                // const xx = await postComment(e,()=>{console.log('got called'); setCommentIsPosting(false)}, obj)
-                // console.log("xx=",xx)
-            console.log("You Commented!")
             }}
 
             py={3}>
@@ -256,7 +197,7 @@ function RBForm (props){
                     <Input
                         id='name'
                         name='name'
-                        defaultValue='Jane Doe'
+                        placeholder='Jane Doe'
                     />
                 </Box>
             </Flex>
